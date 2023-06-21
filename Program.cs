4a47f99078d92301069bc08954795f4d4fcfcddc
Gabriel Rodrigues <gv.rodrigues00@gmail.com>
2023-06-21 02:35:16 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GerarSenha
+{
+    internal class GeradorDeSenha
+    {
+        static void Main()
+        {
+            Console.WriteLine("Quantos caracteres a senha deve possuir?: ");
+            int passwordLenght = Convert.ToInt32(Console.ReadLine());
+
+            string password = GeneratePassword(passwordLenght);
+            Console.Clear();
+            Console.WriteLine("Senha gerada: ");
+            Console.WriteLine(password);
+            Console.ReadLine();
+            Environment.Exit(0);
+            
+        }
+
+        static string GeneratePassword(int passwordLenght)
+        {
+            const string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*+-?";
+            Random random = new Random();
+            char[] password = new char[passwordLenght];
+
+            for (int i = 0; i < passwordLenght; i++)
+            {
+                password[i] = allowedCharacters[random.Next(allowedCharacters.Length)];
+            }
+
+            return new string(password);
+        }
+    }
+}
